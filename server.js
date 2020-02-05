b145ba2601ad59f873d2c16b5d7b4d30fdd84e0e
milad esmailpour <mahdi.esmaeelpour@georgebrown.ca>
2020-02-05 16:25:47 -0500

    upload => Configuration server! => Setup the app and routers

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-require('dotenv').config()
+//importing requirement
+/////////////////////////////////////////////////////
+require('dotenv').config() //import to use .env file
+
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const app = express()
 
+//////////////////////////////////////////////////////
 
+//Connecting to mongodb and comfim the connection and show the massage or show the error
+// DATABASE_UR initialized in .env
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database!'))
 
+// Setup the app and Router
+app.use(express.json())
+const subscriberasRouter = require()
 
+// Listening to port 3000 to check server ran
 app.listen(3000, () => console.log('Server Started on Port 3000'))
